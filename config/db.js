a16d1dcc5856fc2b4c39c8fc8b40edf7a95a392e
Techsrivastava <Adarsh00761@gmail.com>
2023-05-15 19:09:52 +0530

    change update

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
 const mongoose = require ('mongoose');
+mongoose.Promise = global.Promise;
 
 
 
 const connection = mongoose.createConnection('mongodb://127.0.0.1:27017/ed-tech').on('open', ()=>{
+    useUnifiedTopology: true
+    useNewUrlParser: true
     console.log('Db conected');
+    
 }).on('error', ()=>{
     console.log('error');
 });
